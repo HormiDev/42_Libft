0152fdef56753cf4caadc5a37bb514743af36966
HormiDev <ivanddiegomegia@gmail.com>
2024-02-11 02:14:28 +0100

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,15 @@
 /*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 22:51:53 by ide-dieg          #+#    #+#             */
-/*   Updated: 2024/02/10 23:11:51 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2024/02/11 02:02:51 by ide-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char	*str;
 	
+	str = malloc(ft_strlen(s) * sizeof(char));
 }
