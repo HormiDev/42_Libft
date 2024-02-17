8f7e40f9d1c62e3cffa48bba282cafe9d842aa8f
HormiDev <ivanddiegomegia@gmail.com>
2024-02-17 21:40:38 +0100

    lstmap  2.0

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,18 +6,18 @@
 /*   By: ide-dieg <ide-dieg@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 22:43:19 by ide-dieg          #+#    #+#             */
-/*   Updated: 2024/02/15 01:22:29 by ide-dieg         ###   ########.fr       */
+/*   Updated: 2024/02/17 21:39:41 by ide-dieg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/*
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*listnew;
 	t_list	*point;
 
-	if (lst == 0 || f == 0)
+	if (lst == 0 || f == 0 || del == 0)
 		return (0);
 	listnew = ft_lstnew(f(lst->content));
 	if (listnew == 0)
@@ -36,6 +36,66 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			return (0);
 		}
 		point = point->next;
+		lst = lst->next;
+	}
+	return (listnew);
+}
+*/
+/*
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*listnew;
+	t_list	*point;
+	void	*buffer;
+
+	if (lst == 0 || f == 0 || del == 0)
+		return (0);
+	buffer = f(lst->content);
+	listnew = ft_lstnew(buffer);
+	if (listnew == 0)
+	{
+		del(buffer);
+		ft_lstclear(&listnew, del);
+		return (0);
+	}
+	point = listnew;
+	lst = lst->next;
+	while (lst != 0)
+	{
+		buffer = f(lst->content);
+		point->next = ft_lstnew(buffer);
+		if (point->next == 0)
+		{
+			del(buffer);
+			ft_lstclear(&listnew, del);
+			return (0);
+		}
+		point = point->next;
+		lst = lst->next;
+	}
+	return (listnew);
+}
+*/
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*listnew;
+	t_list	*point;
+	void	*buffer;
+
+	if (lst == 0 || f == 0 || del == 0)
+		return (0);
+	listnew = 0;
+	while (lst != 0)
+	{
+		buffer = f(lst->content);
+		point = ft_lstnew(buffer);
+		if (point == 0)
+		{
+			del(buffer);
+			ft_lstclear(&listnew, del);
+			return (0);
+		}
+		ft_lstadd_back(&listnew, point);
 		lst = lst->next;
 	}
 	return (listnew);
